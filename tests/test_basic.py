2f55ac5bd8c5fdee4e32e9cbaafae04f917afd90
Carlos Casado <carlos.casadog@estudiantes.upm.es>
2026-04-27 14:47:44 +0200

    updated test name to fit standard

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 # tests/test_basic.py
 
-def basic_initial_test():
+def test_init():
     """Un test de ejemplo para asegurar que el pipeline funciona."""
     # Aquí en el futuro probaremos tu get_next_state()
     # Por ahora, solo le decimos al robot: "1 + 1 es 2, todo está bien"
